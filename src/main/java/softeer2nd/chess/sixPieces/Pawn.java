efc528a1589a46491ad6cf0ab7135da7b3146c9e
Hypersand <on8214@gmail.com>
2023-07-03 17:45:01 +0900

    Pawn Class 생성

---
+++ src/main/java/softeer2nd/chess/sixPieces/Pawn.java
@@ -0,0 +1,14 @@
+package softeer2nd.chess.sixPieces;
+
+public class Pawn {
+
+    private String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
