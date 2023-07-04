149716b51e172189826e6726e2427fa71ea92c1a
Hypersand <on8214@gmail.com>
2023-07-04 10:32:25 +0900

    feat: Board 클래스 작성 및 findPawn 메서드 구현

---
+++ src/main/java/softeer2nd/chess/Board.java
@@ -0,0 +1,15 @@
+package softeer2nd.chess;
+
+import softeer2nd.chess.sixPieces.Pawn;
+
+import java.util.ArrayList;
+
+public class Board extends ArrayList<Pawn> {
+
+    public Board() {
+    }
+
+    public Pawn findPawn(int index) {
+        return this.get(index);
+    }
+}
