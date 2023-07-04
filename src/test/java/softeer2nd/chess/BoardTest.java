e435dffcfce8d02fe70153ab58c5d0ec15f8ff6b
Hypersand <on8214@gmail.com>
2023-07-04 17:51:26 +0900

    test: initialize 메서드 테스트 코드 작성

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import softeer2nd.chess.pieces.Pawn;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
@@ -13,28 +15,53 @@ class BoardTest {
     @BeforeEach
     public void createBoard() {
         board = new Board();
+        for (int i = 0; i < 8; i++) {
+            board.add(new ArrayList<>());
+        }
     }
 
     @Test
     public void create() throws Exception {
 
-        Pawn white = createPawnAndAddToBoard(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        verifyBoard(white, 1, 0);
+        Pawn white = createPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        addToBoard(white);
+        verifyBoard(white, 1, 1, 0);
 
-        Pawn black = createPawnAndAddToBoard(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
-        verifyBoard(black, 2, 1);
+        Pawn black = createPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
+        addToBoard(black);
+        verifyBoard(black, 1, 6, 0);
     }
 
-    private Pawn createPawnAndAddToBoard(String color, char representation) {
+    Pawn createPawn(String color, char representation) {
         Pawn pawn = new Pawn(color, representation);
-        board.add(pawn);
-
         return pawn;
     }
 
-    private void verifyBoard(Pawn pawn, int size, int index) {
-        assertEquals(size, board.size());
-        assertEquals(pawn, board.findPawn(index));
+    void addToBoard(Pawn pawn) {
+        if (pawn.getColor().equals(Pawn.WHITE_COLOR)) {
+            if (board.get(1).size() < 8) {
+                board.get(1).add(pawn);
+            }
+        }
+
+        else {
+            if (board.get(6).size() < 8) {
+                board.get(6).add(pawn);
+            }
+        }
+    }
+
+    private void verifyBoard(Pawn pawn, int size, int row, int col) {
+        assertEquals(size, board.get(row).size());
+        assertEquals(pawn, board.findPawn(row, col));
+    }
+
+    @Test
+    public void initialize() throws Exception {
+        Board board = new Board();
+        board.initialize();
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
     }
 
 }
