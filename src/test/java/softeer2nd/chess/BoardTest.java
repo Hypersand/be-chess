9972628135e98518164538266c5eb8f92b864379
Hypersand <on8214@gmail.com>
2023-07-05 11:47:43 +0900

    test: 전체 체스판의 상태를 확인하는 테스트

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -2,63 +2,35 @@ package softeer2nd.chess;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import softeer2nd.chess.pieces.Pawn;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static softeer2nd.utils.StringUtils.appendNewLine;
 
 class BoardTest {
 
-    Board board;
+    private Board board;
 
     @BeforeEach
-    public void createBoard() {
+    public void setUp() {
         board = new Board();
         board.initialize();
     }
 
     @Test
     public void create() throws Exception {
-
-        board = new Board();
-        for (int i = 0; i < 8; i++) {
-            board.add(new ArrayList<>());
-        }
-
-        Pawn white = createPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
-        addToBoard(white);
-        verifyBoard(white, 1, 1, 0);
-
-        Pawn black = createPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
-        addToBoard(black);
-        verifyBoard(black, 1, 6, 0);
+        assertEquals(32, board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertEquals(
+                        appendNewLine("RNBQKBNR") +
+                        appendNewLine("PPPPPPPP") +
+                        blankRank + blankRank + blankRank + blankRank +
+                        appendNewLine("pppppppp") +
+                        appendNewLine("rnbqkbnr"),
+                        board.showBoard()
+        );
     }
 
-    private Pawn createPawn(String color, char representation) {
-        Pawn pawn = new Pawn(color, representation);
-        return pawn;
-    }
 
-    private void addToBoard(Pawn pawn) {
-        if (pawn.getColor().equals(Pawn.WHITE_COLOR)) {
-            if (board.get(1).size() < 8) {
-                board.get(1).add(pawn);
-            }
-        }
-
-        else {
-            if (board.get(6).size() < 8) {
-                board.get(6).add(pawn);
-            }
-        }
-    }
-
-    private void verifyBoard(Pawn pawn, int size, int row, int col) {
-
-        assertEquals(size, board.get(row).size());
-        assertEquals(pawn, board.findPawn(row, col));
-    }
 
     @Test
     public void initialize() throws Exception {
@@ -84,14 +56,14 @@ class BoardTest {
     @Test
     public void print() {
 
-        String expectPrint = "........" + "\n"
-                + "pppppppp" + "\n"
-                + "........" + "\n"
-                + "........" + "\n"
-                + "........" + "\n"
-                + "........" + "\n"
+        String expectPrint = "RNBQKBNR" + "\n"
                 + "PPPPPPPP" + "\n"
-                + "........";
+                + "........" + "\n"
+                + "........" + "\n"
+                + "........" + "\n"
+                + "........" + "\n"
+                + "pppppppp" + "\n"
+                + "rnbqkbnr" + "\n";
         assertEquals(expectPrint, board.toString());
     }
 
