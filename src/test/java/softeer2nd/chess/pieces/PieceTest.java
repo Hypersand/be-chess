6594b787802bb04567375db95218dfdef1f4664f
Hypersand <on8214@gmail.com>
2023-07-05 17:34:29 +0900

    test: 팩토리 메서드 변경에 따른 테스트 코드 수정

--- src/test/java/softeer2nd/chess/pieces/PieceTest.java
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -2,6 +2,8 @@ package softeer2nd.chess.pieces;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import softeer2nd.chess.pieces.Piece.Color;
+import softeer2nd.chess.pieces.Piece.Type;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,29 +12,31 @@ class PieceTest {
     @Test
     @DisplayName("팩토리 메서드로 기물 생성 시 정상적으로 만들어져야 한다.")
     public void create_piece() {
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.PAWN), Piece.Color.WHITE, Piece.Type.PAWN);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.PAWN), Piece.Color.BLACK, Piece.Type.PAWN);
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.KNIGHT), Piece.Color.WHITE, Piece.Type.KNIGHT);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.KNIGHT), Piece.Color.BLACK, Piece.Type.KNIGHT);
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.ROOK), Piece.Color.WHITE, Piece.Type.ROOK);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.ROOK), Piece.Color.BLACK, Piece.Type.ROOK);
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.BISHOP), Piece.Color.WHITE, Piece.Type.BISHOP);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.BISHOP), Piece.Color.BLACK, Piece.Type.BISHOP);
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.KING), Piece.Color.WHITE, Piece.Type.KING);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.KING), Piece.Color.BLACK, Piece.Type.KING);
-        verifyPiece(Piece.createPiece(Piece.Color.WHITE, Piece.Type.QUEEN), Piece.Color.WHITE, Piece.Type.QUEEN);
-        verifyPiece(Piece.createPiece(Piece.Color.BLACK, Piece.Type.QUEEN), Piece.Color.BLACK, Piece.Type.QUEEN);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.PAWN), Piece.createPiece(Color.BLACK, Type.PAWN), Type.PAWN);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.KNIGHT), Piece.createPiece(Color.BLACK, Type.KNIGHT), Type.KNIGHT);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.ROOK), Piece.createPiece(Color.BLACK, Type.ROOK), Type.ROOK);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.BISHOP), Piece.createPiece(Color.BLACK, Type.BISHOP), Type.BISHOP);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.KING), Piece.createPiece(Color.BLACK, Type.KING), Type.KING);
+        verifyPiece(Piece.createPiece(Color.WHITE, Type.QUEEN), Piece.createPiece(Color.BLACK, Type.QUEEN), Type.QUEEN);
+
+        Piece blank = Piece.createBlank();
+        assertFalse(blank.isWhite());
+        assertFalse(blank.isBlack());
+        assertEquals(Type.NO_PIECE, blank.getType());
     }
 
-    private void verifyPiece(final Piece piece, Piece.Color color, Piece.Type type) {
-        assertEquals(color, piece.getColor());
-        assertEquals(type, piece.getType());
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, Type type) {
+        assertTrue(whitePiece.isWhite());
+        assertEquals(type, whitePiece.getType());
+
+        assertTrue(blackPiece.isBlack());
+        assertEquals(type, blackPiece.getType());
     }
 
     @Test
     @DisplayName("Piece가 검은색 말인지 구분할 수 있어야 한다.")
     public void isBlack() {
-        Piece piece = Piece.createPiece(Piece.Color.BLACK, Piece.Type.KING);
+        Piece piece = Piece.createPiece(Color.BLACK, Type.KING);
         assertTrue(piece.isBlack());
         assertFalse(piece.isWhite());
     }
@@ -40,15 +44,15 @@ class PieceTest {
     @Test
     @DisplayName("Piece가 흰색 말인지 구분할 수 있어야 한다.")
     public void isWhite() {
-        Piece piece = Piece.createPiece(Piece.Color.WHITE, Piece.Type.QUEEN);
+        Piece piece = Piece.createPiece(Color.WHITE, Type.QUEEN);
         assertTrue(piece.isWhite());
         assertFalse(piece.isBlack());
     }
 
     @Test
     public void getRepresentationPerPiece() throws Exception {
-        assertEquals('p', Piece.Type.PAWN.getWhiteRepresentation());
-        assertEquals('P', Piece.Type.PAWN.getBlackRepresentation());
+        assertEquals('p', Type.PAWN.getWhiteRepresentation());
+        assertEquals('P', Type.PAWN.getBlackRepresentation());
     }
 
 }
