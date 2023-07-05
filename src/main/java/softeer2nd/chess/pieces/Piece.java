2a923648e71cc77a538a3e927c6cd3745297a254
Hypersand <on8214@gmail.com>
2023-07-05 11:53:18 +0900

    feat: Piece의 색을 구분하는 isBlack, isWhite 메서드 생성

--- src/main/java/softeer2nd/chess/pieces/Piece.java
+++ src/main/java/softeer2nd/chess/pieces/Piece.java
@@ -1,22 +1,6 @@
 package softeer2nd.chess.pieces;
 
 public class Piece {
-
-    public static final String WHITE_COLOR = "white";
-    public static final String BLACK_COLOR = "black";
-    public static final char WHITE_PAWN_REPRESENTATION = 'p';
-    public static final char BLACK_PAWN_REPRESENTATION = 'P';
-    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
-    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
-    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
-    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
-    public static final char WHITE_ROOK_REPRESENTATION = 'r';
-    public static final char BLACK_ROOK_REPRESENTATION = 'R';
-    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
-    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
-    public static final char WHITE_KING_REPRESENTATION = 'k';
-    public static final char BLACK_KING_REPRESENTATION = 'K';
-
     private final String color;
     private final char representation;
 
@@ -33,6 +17,29 @@ public class Piece {
     public char getRepresentation() {
         return representation;
     }
+
+    public boolean isBlack() {
+        return color.equals(BLACK_COLOR);
+    }
+
+    public boolean isWhite() {
+        return color.equals(WHITE_COLOR);
+    }
+
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_ROOK_REPRESENTATION = 'r';
+    public static final char BLACK_ROOK_REPRESENTATION = 'R';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
 
     public static Piece createWhitePawn() {
         return new Piece(WHITE_COLOR, WHITE_PAWN_REPRESENTATION);
