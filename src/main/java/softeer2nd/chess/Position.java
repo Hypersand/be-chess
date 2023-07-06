c2981d32d040aa85a476e572682be3d58a133d73
Hypersand <on8214@gmail.com>
2023-07-06 17:16:28 +0900

    feat: Position 클래스 추가

---
+++ src/main/java/softeer2nd/chess/Position.java
@@ -0,0 +1,14 @@
+package softeer2nd.chess;
+
+public class Position {
+
+    private final String position;
+
+    public Position(String position) {
+        this.position = position;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
