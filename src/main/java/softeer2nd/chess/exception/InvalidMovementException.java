30449660d47f5605ee8a243d0c84b92b416856af
Hypersand <on8214@gmail.com>
2023-07-07 13:36:19 +0900

    feat: King 기물에 대한 예외처리 구현

---
+++ src/main/java/softeer2nd/chess/exception/InvalidMovementException.java
@@ -0,0 +1,7 @@
+package softeer2nd.chess.exception;
+
+public class InvalidMovementException extends RuntimeException {
+    public InvalidMovementException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/softeer2nd/chess/exception/InvalidSameColorException.java
@@ -0,0 +1,7 @@
+package softeer2nd.chess.exception;
+
+public class InvalidSameColorException extends RuntimeException {
+    public InvalidSameColorException(String message) {
+        super(message);
+    }
+}
