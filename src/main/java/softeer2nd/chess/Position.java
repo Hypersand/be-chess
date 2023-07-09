12e8cd29d4126a2ad75f97f35394cd15e63f1854
Hypersand <on8214@gmail.com>
2023-07-09 21:35:47 +0900

    refactor: 변수명 확실하게 변경 및 Position 생성자 추가

--- src/main/java/softeer2nd/chess/Position.java
+++ src/main/java/softeer2nd/chess/Position.java
@@ -1,49 +1,60 @@
 package softeer2nd.chess;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Position {
 
-    private int x;
-    private int y;
+    public static final int BOARD_LENGTH = 8;
+
+    private int rank;
+    private int file;
 
     public Position(String position) {
 
         Map<String, Integer> posMap = calculatePosition(position);
 
-        this.x = posMap.get("x");
-        this.y = posMap.get("y");
+        this.rank = posMap.get("rank");
+        this.file = posMap.get("file");
+    }
+
+    public Position(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
+    }
+
+    public void movePosition(String position) {
+        Map<String, Integer> posMap = calculatePosition(position);
+        this.rank = posMap.get("rank");
+        this.file = posMap.get("file");
     }
 
     private Map<String, Integer> calculatePosition(String position) {
 
         Map<String, Integer> map = new HashMap<>();
 
-        String[] px_py = position.split("");
-        int pos_x = px_py[0].charAt(0) - 'a';
-        int pos_y = 8 - Integer.parseInt(px_py[1]);
+        String[] positions = position.split("");
+        int position_rank = BOARD_LENGTH - Integer.parseInt(positions[1]);
+        int position_file = positions[0].charAt(0) - 'a';
 
-        map.put("x", pos_x);
-        map.put("y", pos_y);
+        map.put("rank", position_rank);
+        map.put("file", position_file);
 
         return map;
     }
 
-    public int getX() {
-        return x;
-    }
+    public List<String> getWhitePawnPosition() {
 
-    public int getY() {
-        return y;
-    }
+        List<String> whitePawnPositionList = new ArrayList<>();
 
+        for (int i = 0; i < BOARD_LENGTH; i++) {
+            String position = String.valueOf((char) ('a' + i)) + (2);
+            whitePawnPositionList.add(position);
+        }
 
-
-    public void movePosition(String position) {
-        Map<String, Integer> posMap = calculatePosition(position);
-        this.x = posMap.get("x");
-        this.y = posMap.get("y");
+        return whitePawnPositionList;
     }
 
 }
