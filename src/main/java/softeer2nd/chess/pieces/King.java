46ba5de6fbcac7ae55b28c538ac8aa647c1dd7dd
Hypersand <on8214@gmail.com>
2023-07-10 17:32:43 +0900

    feat: King의 verifyMovePosition 로직 작성

--- src/main/java/softeer2nd/chess/pieces/King.java
+++ src/main/java/softeer2nd/chess/pieces/King.java
@@ -2,6 +2,7 @@ package softeer2nd.chess.pieces;
 
 import softeer2nd.chess.Position;
 import softeer2nd.chess.exception.InvalidMovementException;
+import softeer2nd.utils.Direction;
 
 public class King extends Piece {
 
@@ -19,11 +20,22 @@ public class King extends Piece {
 
     @Override
     public void verifyMovePosition(Position sourcePosition, Position targetPosition) {
-        if (Math.abs(sourcePosition.getRank() - targetPosition.getRank()) <= 1
-                && Math.abs(sourcePosition.getFile() - targetPosition.getFile()) <= 1) {
-            return;
+        int xDist = targetPosition.getFile() - sourcePosition.getFile();
+        int yDist = targetPosition.getRank() - sourcePosition.getRank();
+
+        boolean canMove = false;
+
+        for (Direction direction : Direction.everyDirection()) {
+            if (direction.getXDegree() == xDist && direction.getYDegree() == yDist) {
+                canMove = true;
+                break;
+            }
         }
-        throw new InvalidMovementException("킹은 한 칸만 이동할 수 있습니다.");
+
+        if (!canMove) {
+            throw new InvalidMovementException("킹은 한 칸만 이동할 수 있습니다.");
+        }
+
     }
 
 
