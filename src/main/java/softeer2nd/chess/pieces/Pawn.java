47ec4ee42cc1e0cc288ba497cca46957cb1e3cbf
Hypersand <on8214@gmail.com>
2023-07-13 00:17:12 +0900

    refactor: Pawn 검증 로직 들여쓰기 줄이기

--- src/main/java/softeer2nd/chess/pieces/Pawn.java
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -19,41 +19,51 @@ public class Pawn extends Piece {
 
     @Override
     public void verifyMovePosition(Position sourcePosition, Position targetPosition) {
-
         int yDist = targetPosition.getRank() - sourcePosition.getRank();
 
         if (isWhite()) {
-            if (sourcePosition.isWhitePawnStartPosition()) {
-                if (targetPosition.getFile() != sourcePosition.getFile()) {
-                    throw new InvalidMovementException("폰은 다른 파일로 이동할 수 없습니다!");
-                }
-
-                if (Math.abs(yDist) > 2) {
-                    throw new InvalidMovementException("시작 위치의 폰은 최대 두 칸 이동할 수 있습니다!");
-                }
-                return;
-            }
-
-            if (yDist != -1) {
-                throw new InvalidMovementException("폰의 이동이 올바르지 않습니다!");
-            }
-
+            verifyWhitePawnStartPosition(sourcePosition, targetPosition, yDist);
+            verifyWhitePawnNormalMovement(yDist);
         }
 
         if (isBlack()) {
-            if (sourcePosition.isBlackPawnStartPosition()) {
-                if (targetPosition.getFile() != sourcePosition.getFile()) {
-                    throw new InvalidMovementException("폰은 다른 파일로 이동할 수 없습니다!");
-                }
+            verifyBlackPawnStartPosition(sourcePosition, targetPosition, yDist);
+            verifyBlackPawnNormalMovement(yDist);
+        }
+    }
 
-                if (Math.abs(yDist) > 2) {
-                    throw new InvalidMovementException("시작 위치의 폰은 최대 두 칸 이동할 수 있습니다!");
-                }
-                return;
+    private void verifyWhitePawnNormalMovement(int yDist) {
+        if (yDist != -1) {
+            throw new InvalidMovementException("폰의 이동이 올바르지 않습니다!");
+        }
+    }
+
+    private void verifyBlackPawnNormalMovement(int yDist) {
+        if (yDist != 1) {
+            throw new InvalidMovementException("폰의 이동이 올바르지 않습니다!");
+        }
+    }
+
+    private void verifyWhitePawnStartPosition(Position sourcePosition, Position targetPosition, int yDist) {
+        if (sourcePosition.isWhitePawnStartPosition()) {
+            if (targetPosition.getFile() != sourcePosition.getFile()) {
+                throw new InvalidMovementException("폰은 다른 파일로 이동할 수 없습니다!");
             }
 
-            if (yDist != 1) {
-                throw new InvalidMovementException("폰의 이동이 올바르지 않습니다!");
+            if (Math.abs(yDist) > 2) {
+                throw new InvalidMovementException("시작 위치의 폰은 최대 두 칸 이동할 수 있습니다!");
+            }
+        }
+    }
+
+    private void verifyBlackPawnStartPosition(Position sourcePosition, Position targetPosition, int yDist) {
+        if (sourcePosition.isWhitePawnStartPosition()) {
+            if (targetPosition.getFile() != sourcePosition.getFile()) {
+                throw new InvalidMovementException("폰은 다른 파일로 이동할 수 없습니다!");
+            }
+
+            if (Math.abs(yDist) > 2) {
+                throw new InvalidMovementException("시작 위치의 폰은 최대 두 칸 이동할 수 있습니다!");
             }
         }
     }
